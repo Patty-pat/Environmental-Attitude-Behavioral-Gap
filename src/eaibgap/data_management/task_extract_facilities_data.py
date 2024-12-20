f58f31232cf6f673b30e0513decaa74fde1db371
Patricia Puspitasari <patricia.suratno@gmail.com>
2024-12-21 00:11:20 +0100

    modify task_extract_facilities_data.py

--- src/eaibgap/data_management/task_extract_facilities_data.py
+++ src/eaibgap/data_management/task_extract_facilities_data.py
@@ -1,12 +1,13 @@
 """Tasks for managing the data."""
 
 import pytask
+from pytask import task
 from eaibgap.config import SRC, BLD
 from eaibgap.data_management.facilities_data_json_to_xls import (
     facilities_data_json_to_xls,
 )
 
-@pytask.mark.task
+@task
 def task_extract_json_to_xls_central_java(
     json_input=SRC / "data" / "central_java.json",
     produces=BLD / "data" / "central_java.xlsx",
